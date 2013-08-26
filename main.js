28ee500a08b4be90df2484200ea48c0ac82415b7
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2013-08-26 21:04:05 +0200

    More initial entities

--- main.js
+++ main.js
@@ -33,10 +33,28 @@ define([
 		}
 	}
 
+	var tower  = [[8, -8, 0]];
+	var ranged = [[7, -7, 0], [7, -8, 1], [8, -7, -1]];
+	var melee  = [[5, -5, 0], [5, -6, 1], [6, -5, -1]];
+
+	function mult(coords, player) {
+		var rval = [];
+		for (var i = 0; i < coords.length; ++i) {
+			rval[i] = coords[i] * player;
+		}
+		return rval;
+	}
+
 	for (var player = -1; player <= 1; player += 2) {
-		board.addEntity([8 * player, -8 * player, 0], new Tower(player));
-		board.addEntity([7 * player, -7 * player, 0], new Ranged(player));
-		board.addEntity([5 * player, -5 * player, 0], new Melee(player));
+		tower.forEach(function(coords) {
+			board.addEntity(mult(coords, player), new Tower(player));
+		});
+		ranged.forEach(function(coords) {
+			board.addEntity(mult(coords, player), new Ranged(player));
+		});
+		melee.forEach(function(coords) {
+			board.addEntity(mult(coords, player), new Melee(player));
+		});
 	}
 
 	document.getElementById('next_turn').onclick = function() {
